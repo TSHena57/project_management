0b18c35610e59a8a17504577537564e014bdfa60
Naimul Islam <naimcse56@gmail.com>
2025-05-02 20:44:21 +0600

    admin templating and sidebar

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -13,21 +13,13 @@
              </li>
           </ul>
        </div>
-       <div class="search-toggle-icon d-xl-none ms-auto">
-          <i class="bi bi-search"></i>
-       </div>
-       <form class="searchbar d-none d-xl-flex ms-auto">
-          <div class="position-absolute top-50 translate-middle-y search-icon ms-3"><i class="bi bi-search"></i></div>
-          <input class="form-control" type="text" placeholder="Type here to search">
-          <div class="position-absolute top-50 translate-middle-y d-block d-xl-none search-close-icon"><i class="bi bi-x-lg"></i></div>
-       </form>
-       <div class="top-navbar-right ms-3">
+       <div class="top-navbar-right d-xl-flex ms-auto">
           <ul class="navbar-nav align-items-center">
              <li class="nav-item dropdown dropdown-large">
                 <a class="nav-link dropdown-toggle dropdown-toggle-nocaret" href="#" data-bs-toggle="dropdown">
                    <div class="user-setting d-flex align-items-center gap-1">
                       <img src="{{asset('images/avatars/avatar-1.png')}}" class="user-img" alt="">
-                      <div class="user-name d-none d-sm-block">{{ Auth::user()->name }}</div>
+                      <div class="user-name d-none d-sm-block">John Dee</div>
                    </div>
                 </a>
                 <ul class="dropdown-menu dropdown-menu-end">
@@ -36,7 +28,7 @@
                          <div class="d-flex align-items-center">
                             <img src="{{asset('images/avatars/avatar-1.png')}}" alt="" class="rounded-circle" width="60" height="60">
                             <div class="ms-3">
-                               <h6 class="mb-0 dropdown-user-name">{{ Auth::user()->name }}</h6>
+                               <h6 class="mb-0 dropdown-user-name">John Dee</h6>
                                <small class="mb-0 dropdown-user-designation text-secondary">Admin</small>
                             </div>
                          </div>
